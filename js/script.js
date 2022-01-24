d76771d4af71b180847577390086d35006eb1d6b
Sujan Mridha <60531496+sujancse5bu@users.noreply.github.com>
2022-01-24 12:33:58 +0600

    initial disc implemented dynamic with input

--- js/script.js
+++ js/script.js
@@ -1,9 +1,46 @@
+const discColors = ['red', 'blue', 'green', 'yellow', 'violet', 'aqua']
+let totalDiscLength = 3;
+let discInfo1 = []
+let discInfo2 = []
+let discInfo3 = []
+
 document.getElementById('tohBtn').addEventListener('click', () => showSection('toh'))
 
+window.onload = () => {
+  generateDiscs1(Array.from({ length: 3 }).map((item, index) => index + 1))
+  document.getElementById('tohInput').addEventListener('change', (e) => {
+    generateDiscs1(Array.from({length: parseInt(e.target.value)}).map((item, index) => index + 1))
+  })
+}
 
+const generateDiscs1 = (discArray) => {
+  const widthdiff = 9 - discArray.length;
+  discInfo2 = []
+  discInfo3 = []
 
+  // discArray.reverse()
+  discInfo1 = discArray.reverse().map((item, index) => ({
+    id: item,
+    color: discColors[item - 1],
+    width: 20 - widthdiff * (index)
+  }))
+  discInfo1 = discInfo1.reverse()
+  updateTOH_UI()
+}
 
-
+const updateTOH_UI = () => {
+  const discContainer1 = document.querySelector('.disc-container.disc-container1')
+  console.log('disc1: ', discInfo1)
+  let data = ''
+  discInfo1.forEach((item, index) => {
+    data += `<div 
+    class="disc-item disc-item${item.id}" 
+    style="width: ${item.width}rem; 
+    background-color: ${item.color}"></div>`
+  })
+  discContainer1.innerHTML = data
+  console.log(data)
+}
 
 const showSection = (targetClass) => {
   const sections = document.getElementsByClassName('content')
