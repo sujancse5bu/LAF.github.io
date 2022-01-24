acc8d6d8254555b9ef51a1a0c7b0b44c534d704b
Sujan Mridha <60531496+sujancse5bu@users.noreply.github.com>
2022-01-24 10:44:28 +0600

    js fixed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-document.getElementById('tohBtn').addEventListener('click', () => window.location.pathname = '/toh.html')
+document.getElementById('tohBtn').addEventListener('click', () => showSection('toh'))
+const showSection = (targetClass) => {
+  const sections = document.getElementsByClassName('content')
+  Array.from(sections, (element, index) => {
+    if (element.classList.contains(targetClass)) {
+      element.style.display = 'initial'
+    } else {
+      element.style.display = 'none'
+    }
+  })
+}
