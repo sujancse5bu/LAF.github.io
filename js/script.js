5c505e7f2fd5c95f909a7e365c75942ae96371b3
Sujan Mridha <60531496+sujancse5bu@users.noreply.github.com>
2022-02-18 13:50:45 +0600

    speed control added

--- js/script.js
+++ js/script.js
@@ -1,24 +1,82 @@
-const discColors = ['red', 'blue', 'green', 'yellow', 'violet', 'aqua']
+const discContainer1 = document.querySelector('.disc-container.disc-container1')
+const discContainer2 = document.querySelector('.disc-container.disc-container2')
+const discContainer3 = document.querySelector('.disc-container.disc-container3')
+const playBtn = document.getElementById('playBtn')
+const stopBtn = document.getElementById('stopBtn')
+const speedUpBtn = document.getElementById('speedUpBtn')
+const speedDownBtn = document.getElementById('speedDownBtn')
+const discColors = ['crimson', 'violet', 'skyblue', 'darkgreen', 'pink', 'blue', 'darkgray', 'greenyellow', 'teal', 'tomato']
 let totalDiscLength = 3
 let discInfos = []
-
-
-document.getElementById('tohBtn').addEventListener('click', () => showSection('toh'))
+let speed = 1
+let speedTime = 1000
+let play = false
+let uiInterval = null
+let uiIndex = -1
 
 window.onload = () => {
-  generateDiscs1(Array.from({ length: 3 }).map((item, index) => index + 1))
+  generateDiscs1(Array.from({ length: totalDiscLength }).map((item, index) => index + 1))
   document.getElementById('tohInput').addEventListener('change', (e) => {
+    play = false
+    playBtn.innerHTML = 'Play'
+    clearInterval(uiInterval)
+    uiIndex = -1
     totalDiscLength = parseInt(e.target.value)
     discInfos = []
     generateDiscs1(Array.from({length: parseInt(e.target.value)}).map((item, index) => index + 1))
   })
-  document.getElementById('startBtn').addEventListener('click', () => {
-    Hanoi(totalDiscLength, 1, 3, 2)
-    setTimeout(() => {
-      updateTOH_UI()
-    }, 1000)
+  playBtn.addEventListener('click', () => {
+    if (!play) {
+      play = true
+      playBtn.innerHTML = 'Pause'
+      if (uiIndex === -1) {
+        discInfos = []
+        generateDiscs1(Array.from({ length: totalDiscLength }).map((item, index) => index + 1))
+        Hanoi(totalDiscLength, 1, 3, 2)
+      }
+      updateTOH_interval()
+    } else {
+      clearInterval(uiInterval)
+      play = false
+      playBtn.innerHTML = 'Play'
+    }
   })
-}//
+  stopBtn.addEventListener('click', () => {
+    play = false
+    playBtn.innerHTML = 'Play'
+    clearInterval(uiInterval)
+    uiIndex = -1
+    discInfos = []
+    generateDiscs1(Array.from({ length: totalDiscLength }).map((item, index) => index + 1))
+  })
+  speedUpBtn.addEventListener('click', () => {
+    if (speed === 0) return
+    if (speed <= 0.9) {
+      speed += 0.1
+      speedSpan.innerHTML = `1 disc/${(speedTime / 1000).toFixed(2)} second`
+    } else {
+      ++speed
+      speedSpan.innerHTML = `${speed.toFixed(2)} disc(s)/second`
+    }
+    
+
+    speedTime = 1000 / speed
+    if (play) updateTOH_interval()
+  })
+  speedDownBtn.addEventListener('click', () => {
+    if (speed < 0.2) return
+    if (speed > 1) {
+      --speed
+      speedSpan.innerHTML = `${speed.toFixed(2)} disc(s)/second`
+    }
+    else {
+      speed -= 0.1
+      speedSpan.innerHTML = `1 disc/${(speedTime / 1000).toFixed(2)} second`
+    }
+    speedTime = 1000 / speed
+    if (play) updateTOH_interval()
+  })
+}
 
 const Hanoi = (n, from, to, via) => {
   if (n == 0) return
@@ -27,53 +85,26 @@ const Hanoi = (n, from, to, via) => {
   Hanoi(n-1, via, to , from)
 }
 const moveDisk = (from, to, via) => {
-  console.log('info: ', from, via, to)
+  // console.log('info: ', from, via, to)
   let lastIndex = discInfos.length - 1
   let lastItem = discInfos[lastIndex]
   let lastItemFrom = discInfos[lastIndex][`d${from}`]
   let lastItemFromFirst = discInfos[lastIndex][`d${from}`][0]
-  let lastItemVia = discInfos[lastIndex][`d${via}`]
-  let lastItemFViaFirst = discInfos[lastIndex][`d${via}`][0]
   let lastItemTo = discInfos[lastIndex][`d${to}`]
-  // console.log('lastItemFrom: ', from, lastItemFrom)
   
-  // console.log('discInfos b:', discInfos)
-  discInfos = [ /// removed disc from + add disc via
+  discInfos = [
     ...discInfos,
     {
       ...lastItem, 
       [`d${from}`]: lastItemFrom.filter((item, index) => index !== 0),
-      // [`d${via}`]: [lastItemFromFirst, ...lastItemVia]
       [`d${to}`]: [lastItemFromFirst, ...lastItemTo]
     }
   ]
-  // lastIndex = discInfos.length - 1
-  // lastItem = discInfos[lastIndex]
-  // lastItemFrom = discInfos[lastIndex][`d${from}`]
-  // lastItemVia = discInfos[lastIndex][`d${via}`]
-  // let lastItemTo = discInfos[lastIndex][`d${to}`]
-  // lastItemFromFirst = discInfos[lastIndex][`d${from}`][0]
-  // lastItemFViaFirst = discInfos[lastIndex][`d${via}`][0]
-  // let lastItemFToFirst = discInfos[lastIndex][`d${to}`][0]
-
-  // discInfos = [ /// removed disc via + add disc to
-  //   ...discInfos,
-  //   {
-  //     ...lastItem, 
-  //     [`d${via}`]: lastItemVia.filter((item, index) => index !== 0),
-  //     [`d${to}`]: [lastItemFViaFirst, ...lastItemTo]
-  //   }
-  // ]
   
-  
-
-  // console.log('discInfos a: ', discInfos)
-
-
 }
 
 const generateDiscs1 = (discArray) => {
-  const widthdiff = 9 - discArray.length
+  const widthdiff = 18 / discArray.length
   let discInfo1 = discArray.reverse().map((item, index) => ({
     id: item,
     color: discColors[item - 1],
@@ -85,65 +116,51 @@ const generateDiscs1 = (discArray) => {
     d2: [], 
     d3: []
   }]
-  updateTOH_UI()
+  updateTOH_interval()
 }
 
-const updateTOH_UI = () => {
-  const discContainer1 = document.querySelector('.disc-container.disc-container1')
-  const discContainer2 = document.querySelector('.disc-container.disc-container2')
-  const discContainer3 = document.querySelector('.disc-container.disc-container3')
-  console.log('disc1: ', discInfos)
-  let index = -1
-  const uiInterval = setInterval(() => { 
-    ++index
-    if (index === discInfos.length) {
-      clearInterval(uiInterval)
-      return
-    }
-    console.log('index: ', index, discInfos[index])
-
-    let data = ''
-    discInfos[index]?.d1.forEach((item, index) => {
-      data += `<div 
-      class="disc-item disc-item${item.id}" 
-      style="width: ${item.width}rem; background-color: ${item.color}"></div>`
-    })
-    discContainer1.innerHTML = data
-    console.log('data1: ', data)
-
-    data = ''
-    discInfos[index]?.d2.forEach((item, index) => {
-      data += `<div 
-      class="disc-item disc-item${item.id}" 
-      style="width: ${item.width}rem; background-color: ${item.color}"></div>`
-    })
-    discContainer2.innerHTML = data
-    console.log('data2: ', data)
-
-    data = ''
-    discInfos[index]?.d3.forEach((item, index) => {
-      data += `<div 
-      class="disc-item disc-item${item.id}" 
-      style="width: ${item.width}rem; background-color: ${item.color}"></div>`
-    })
-    discContainer3.innerHTML = data
-    console.log('data3: ', data)
-
-    // clearInterval(uiInterval)
-    // return
-  }, 1000)
+const updateTOH_interval = () => {
+  clearInterval(uiInterval)
+  if (uiIndex === -1) updateTOH()
+  uiInterval = setInterval(() => { 
+    updateTOH()
+  }, speedTime)
   
-  console.log(discContainer1)
 }
 
-const showSection = (targetClass) => {
-  const sections = document.getElementsByClassName('content')
-  Array.from(sections, (element, index) => {
-    if (element.classList.contains(targetClass)) {
-      element.style.display = 'initial'
-    } else {
-      element.style.display = 'none'
-    }
+
+const updateTOH = () => {
+  ++uiIndex
+  if (uiIndex === discInfos.length) {
+    clearInterval(uiInterval)
+    uiIndex = -1
+    play = false
+    playBtn.innerHTML = 'Play'
+    return
+  }
+  
+  let data = ''
+  discInfos[uiIndex]?.d1.forEach((item, index) => {
+    data += `<div 
+    class="disc-item disc-item${item.id}" 
+    style="width: ${item.width}rem; background-color: ${item.color}"></div>`
   })
+  discContainer1.innerHTML = data
+
+  data = ''
+  discInfos[uiIndex]?.d2.forEach((item, index) => {
+    data += `<div 
+    class="disc-item disc-item${item.id}" 
+    style="width: ${item.width}rem; background-color: ${item.color}"></div>`
+  })
+  discContainer2.innerHTML = data
+
+  data = ''
+  discInfos[uiIndex]?.d3.forEach((item, index) => {
+    data += `<div 
+    class="disc-item disc-item${item.id}" 
+    style="width: ${item.width}rem; background-color: ${item.color}"></div>`
+  })
+  discContainer3.innerHTML = data
 }
 
